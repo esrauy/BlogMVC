17a15d90f38e5b3fcc56fae6f84c0fe47be888d6
Esra <121902370+esrauy@users.noreply.github.com>
2023-01-08 11:50:43 +0300

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
 // denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
 // bu öznitelik değerlerini değiştirin.
-[assembly: AssemblyTitle("Blog_Common")]
+[assembly: AssemblyTitle("Blog_Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blog_Common")]
+[assembly: AssemblyProduct("Blog_Core")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
-[assembly: Guid("6e99a154-6e50-42d3-a027-735bd1f43b07")]
+[assembly: Guid("581c83a8-c555-4b63-9e2d-fa196945b037")]
 
 // Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
 //
---
+++ IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Core
+{
+    public interface IRepository<T>
+    {
+        //birden fazla kayıt almak için
+        List<T> List();
+        //tek bir kayıt geriye dönmesi için
+        List<T> List(Expression<Func<T, bool>> filter);
+        IQueryable<T> ListQueryable();
+        IQueryable<T> ListQueryable(Expression<Func<T, bool>> filter);
+
+        T GetById(int id);
+        T Find(Expression<Func<T, bool>> filter);
+        int Insert(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        int Save();
+    }
+}
