0b9cff0512e10fb877c30b02327f1daa2d4766d0
Esra <121902370+esrauy@users.noreply.github.com>
2023-01-08 11:49:57 +0300

    Add files via upload

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
 // denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
 // bu öznitelik değerlerini değiştirin.
-[assembly: AssemblyTitle("Blog_BusinessLayer")]
+[assembly: AssemblyTitle("Blog_Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Blog_BusinessLayer")]
+[assembly: AssemblyProduct("Blog_Common")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
-[assembly: Guid("718bc123-01b9-4d32-be4e-79c00e44fd70")]
+[assembly: Guid("6e99a154-6e50-42d3-a027-735bd1f43b07")]
 
 // Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
 //
---
+++ ConfigHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Common.Helpers
+{
+    public class ConfigHelper
+    {
+        public string Get1(string key)
+        {           //web.config içine giriyor. Tanımlanan değerlere key veriyouz parametre olarak, bize value döndürüyor
+            return ConfigurationManager.AppSettings[key];
+        }
+        public static T Get<T>(string key)
+        {
+            return (T)Convert.ChangeType(ConfigurationManager.AppSettings[key], typeof(T));
+        }
+    }
+}
